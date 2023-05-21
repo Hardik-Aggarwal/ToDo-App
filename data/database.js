934a0baf2cb5760c15331c65703914ac1c527324
Hardik Aggarwal <hardik@Hardiks-MacBook-Pro.local>
2023-05-21 12:09:10 +0530

    Fixed engine

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export const connectDB = ()=>{
-    mongoose.connect(process.env.MONGO_URI).then(()=>{console.log("Database Connected")}).catch((err)=>{
+    mongoose.connect(process.env.MONGO_URI).then((c)=>{console.log(`Database Connected on ${c.connection.host}`)}).catch((err)=>{
     console.log(err);
 });
 };
